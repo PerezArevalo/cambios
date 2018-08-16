ec62252a73fbe259582c6df51979d7bf6ab87b4a
Imelda Arevalo <imeldarevalo1020@gmail.com>
2018-08-16 15:38:42 -0600

    ejercicio

---
+++ Daly Arevalo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Daly_Arevalo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
